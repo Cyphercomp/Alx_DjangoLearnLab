9a6383b0fe3968d3bf1713f1e3111d2824350009
Cyphercomp <ndicunguyecedric@gmail.com>
2024-08-25 13:16:59 -0700

    update to views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,6 +14,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from models import Book
+from forms import BookForm
+
 
 # Create your views here.
 def book_list_view(request):
