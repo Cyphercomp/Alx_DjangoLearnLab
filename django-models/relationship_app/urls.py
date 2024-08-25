c2f4bf3209c019ceb03aa17023129d5db5bee680
Cyphercomp <ndicunguyecedric@gmail.com>
2024-08-25 13:28:30 -0700

    update urls

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import list_books, LibraryDetailView
 from django.urls import path
 from .views import admin_view, librarian_view, member_view
-
+from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
     path('', views.book_list_view, name='home'),
